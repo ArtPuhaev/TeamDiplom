d65908a407c5ee27734a30631c62431685d38230
AlinaYaroslavovna <alina.yaroslavovna@gmail.com>
2025-04-26 10:53:54 +0300

    Update SavingAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -1,21 +1,73 @@
 package ru.netology.javaqadiplom;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SavingAccountTest {
+    private SavingAccount account;
+
+    @BeforeEach
+    public void setUp() {
+        account = new SavingAccount(1000, 0, 5000, 10);
+    }
 
     @Test
-    public void shouldAddLessThanMaxBalance() {
-        SavingAccount account = new SavingAccount(
-                2_000,
-                1_000,
-                10_000,
-                5
-        );
+    public void testInitialBalance() {
+        assertEquals(1000, account.getBalance());
+    }
 
-        account.add(3_000);
+    @Test
+    public void testAddPositiveAmount() {
+        assertTrue(account.add(500));
+        assertEquals(1500, account.getBalance());
+    }
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    @Test
+    public void testAddExceedingMaxBalance() {
+        assertFalse(account.add(5000)); 
+        assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void testAddNegativeAmount() {
+        assertFalse(account.add(-100));
+        assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void testPayValidAmount() {
+        assertTrue(account.pay(500));
+        assertEquals(500, account.getBalance());
+    }
+
+    @Test
+    public void testPayExceedingBalance() {
+        assertFalse(account.pay(1500)); 
+        assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void testPayNegativeAmount() {
+        assertFalse(account.pay(-100));
+        assertEquals(1000, account.getBalance());
+    }
+
+    @Test
+    public void testYearChangeCalculation() {
+        int expectedInterest = 100; 
+        assertEquals(expectedInterest, account.yearChange());
+    }
+
+    @Test
+    public void testConstructorWithInvalidParameters() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(1000, 2000, 1500, 10); 
+        });
+        
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(1000, 0, 5000, -5); 
+        });
     }
 }
